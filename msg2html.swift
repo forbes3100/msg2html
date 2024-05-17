73775820f12831ffd0847f1d3d0eb8066ea0e357
Scott Forbes <forbes3100@gmail.com>
2024-05-17 09:24:00 -0700

    Moved chat.db-reading code to msg_db.swift.

--- msg2html.swift
+++ msg2html.swift
@@ -13,7 +13,6 @@
 // GNU General Public License for more details.
 
 import Foundation
-import SQLite3
 
 let debug = 0
 var haveLinks: Bool = false
@@ -141,6 +140,7 @@ struct CSS {
 }
 
 class Message {
+    var who: String?
     var rowid: Int
     var date: Date
     var guid: String
@@ -150,8 +150,9 @@ class Message {
     var text: String?
     var svc: String
 
-    init(rowid: Int, date: Date, guid: String, isFromMe: Bool, hasAttach: Bool,
+    init(who: String?, rowid: Int, date: Date, guid: String, isFromMe: Bool, hasAttach: Bool,
          handleID: Int, text: String?, svc: String) {
+        self.who = who
         self.rowid = rowid
         self.date = date
         self.guid = guid
@@ -160,44 +161,6 @@ class Message {
         self.handleID = handleID
         self.text = text
         self.svc = svc
-    }
-}
-
-func getChatDBCopyPath(toDir msgsBakDirPath: String) -> String {
-    let fileManager = FileManager.default
-    let homeDir = fileManager.homeDirectoryForCurrentUser
-    let originalFilePath = homeDir.appendingPathComponent("Library/Messages/chat.db").path
-    let copyFilePath = msgsBakDirPath + "chat_copy.db"
-
-    // Check if the copy file already exists and has the same modification date as the original file
-    if fileManager.fileExists(atPath: copyFilePath) {
-        do {
-            let originalAttributes = try fileManager.attributesOfItem(atPath: originalFilePath)
-            let copyAttributes = try fileManager.attributesOfItem(atPath: copyFilePath)
-            let originalModificationDate = originalAttributes[.modificationDate] as! Date
-            let copyModificationDate = copyAttributes[.modificationDate] as! Date
-            if originalModificationDate == copyModificationDate {
-                return copyFilePath
-            }
-        } catch {
-            fatalError("Failed to get file attributes: \(error)")
-        }
-    }
-
-    // remove any existing copy of the file
-    try? fileManager.removeItem(atPath: copyFilePath)
-
-    // attempt to copy the file to the Documents directory
-    do {
-        try fileManager.copyItem(atPath: originalFilePath, toPath: copyFilePath)
-
-        // set read permissions on the copied file
-        let fileAttributes = [FileAttributeKey.posixPermissions: NSNumber(value: 0o644)]
-        try fileManager.setAttributes(fileAttributes, ofItemAtPath: copyFilePath)
-
-        return copyFilePath
-    } catch {
-        fatalError("Failed to create chat database copy: \(error)")
     }
 }
 
@@ -216,18 +179,6 @@ class HTML {
 
     func appendMessagesFor(year: Int, msgsBakDirPath: String, extAttDir: String? = nil) {
         let fileManager = FileManager.default
-        let handlesName = "chat_handles.json"
-        let handlesPath = msgsBakDirPath + handlesName
-        var idNamedHandles: [String:String] = [:]
-        if fileManager.fileExists(atPath: handlesPath) {
-            do {
-                let url = URL(fileURLWithPath: handlesPath)
-                let data = try Data(contentsOf: url)
-                idNamedHandles = try JSONSerialization.jsonObject(with: data, options: []) as! [String: String]
-            } catch {
-                fatalError("Reading database handles file \(handlesPath)")
-            }
-        }
 
         if let extAttDir = extAttDir, fileManager.fileExists(atPath: extAttDir) {
             var files = [String]()
@@ -241,103 +192,43 @@ class HTML {
             }
         }
 
-        let chatDBFilePath = getChatDBCopyPath(toDir: msgsBakDirPath)
-        var db: OpaquePointer?
+        let msgSrc = MessageSource()
+        msgSrc.open(msgsBakDirPath: msgsBakDirPath)
+        let messages = msgSrc.getMessagesFor(year: year)
 
-        guard sqlite3_open_v2(chatDBFilePath, &db, SQLITE_OPEN_READWRITE, nil) == SQLITE_OK else {
-            let errorMessage = String(cString: sqlite3_errmsg(db))
-            fatalError("Opening database \(chatDBFilePath): \(errorMessage)")
-        }
-        defer { sqlite3_close(db) }
+        var prevDay = 0
+        var prevWho: String? = nil
 
-        var statement: OpaquePointer?
-        let query = "SELECT rowid, id FROM handle;"
-        if sqlite3_prepare_v2(db, query, -1, &statement, nil) == SQLITE_OK {
-            defer { sqlite3_finalize(statement) }
+        for msg in messages {
 
-            var handles = [Int: String]()
-            while sqlite3_step(statement) == SQLITE_ROW {
-                let rowid = Int(sqlite3_column_int(statement, 0))
-                let id = String(cString: sqlite3_column_text(statement, 1))
-                handles[rowid] = idNamedHandles[id, default: id]
+            let calendar = Calendar.current
+            if calendar.component(.year, from: msg.date) != year {
+                continue
             }
 
-            let messageQuery = """
-            SELECT rowid, datetime(substr(date, 1, 9) + 978307200, 'unixepoch',
-            'localtime') AS f_date, guid, is_from_me, cache_has_attachments,
-            handle_id, text, service
-            FROM message ORDER BY f_date;
-            """
-            var statement2: OpaquePointer?
-            if sqlite3_prepare_v2(db, messageQuery, -1, &statement2,
-                                  nil) == SQLITE_OK {
-                defer { sqlite3_finalize(statement2) }
+            let day = calendar.ordinality(of: .day, in: .year, for: msg.date) ?? -1
+            if day != prevDay {
+                append(tag: "hr")
+            }
 
-                var prevDay = 0
-                var prevWho: String? = nil
-
-                while sqlite3_step(statement2) == SQLITE_ROW {
-                    let rowid = Int(sqlite3_column_int(statement2, 0))
-                    let dateString = String(cString: sqlite3_column_text(statement2, 1))
-                    let guid = String(cString: sqlite3_column_text(statement2, 2))
-                    let isFromMe = sqlite3_column_int(statement2, 3) == 1
-                    let hasAttach = sqlite3_column_int(statement2, 4) == 1
-                    let handleId = Int(sqlite3_column_int(statement2, 5))
-                    var text: String? = nil
-                    if let t = sqlite3_column_text(statement2, 6) {
-                        text = String(cString: t)
-                    }
-                    let svc = String(cString: sqlite3_column_text(statement2, 7))
-
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-                    let date = dateFormatter.date(from: dateString)!
-
-                    let msg = Message(
-                        rowid: rowid,
-                        date: date,
-                        guid: guid,
-                        isFromMe: isFromMe,
-                        hasAttach: hasAttach,
-                        handleID: handleId,
-                        text: text,
-                        svc: svc
-                    )
-
-                    let calendar = Calendar.current
-                    if calendar.component(.year, from: date) != year {
-                        continue
-                    }
-
-                    if msg.handleID == 0 {
-                        msg.isFromMe = true
-                    }
-
-                    let day = calendar.ordinality(of: .day, in: .year, for: date) ?? -1
-                    if day != prevDay {
-                        append(tag: "hr")
-                    }
-
-                    let who = handles[msg.handleID]
-                    css = CSS(isFromMe: msg.isFromMe, svc: msg.svc)
-                    if msg.isFromMe {
-                        append(tag: "p", attributes: ["class": "d"], content: """
-                                   \(msg.date) - from me, \(msg.svc)
-                                   #\(msg.rowid)
-                                   """)
-                    } else {
-                        append(tag: "p", attributes: ["class": "d"], content: """
-                                   \(msg.date) - from \(who ?? "Unknown"), \(msg.svc)
-                                   #\(msg.rowid)
-                                   """)
-                        if who != prevWho || day != prevDay {
-                            append(tag: "p", attributes: ["class": "n"], content: who ?? "Unknown")
-                            prevWho = who
-                        }
-                    }
-                    prevDay = day
+            css = CSS(isFromMe: msg.isFromMe, svc: msg.svc)
+            if msg.isFromMe {
+                append(tag: "p", attributes: ["class": "d"], content: """
+                           \(msg.date) - from me, \(msg.svc)
+                           #\(msg.rowid)
+                           """)
+            } else {
+                let who = msg.who
+                append(tag: "p", attributes: ["class": "d"], content: """
+                           \(msg.date) - from \(who ?? "Unknown"), \(msg.svc)
+                           #\(msg.rowid)
+                           """)
+                if who != prevWho || day != prevDay {
+                    append(tag: "p", attributes: ["class": "n"], content: who ?? "Unknown")
+                    prevWho = who
                 }
             }
+            prevDay = day
         }
     }
 
---
+++ msg_db.swift
@@ -0,0 +1,166 @@
+// msg_db -- Read from a Mac Messages database
+//
+// Copyright (C) 2024 Scott Forbes
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+import Foundation
+import SQLite3
+
+func getChatDBCopyPath(toDir msgsBakDirPath: String) -> String {
+    let fileManager = FileManager.default
+    let homeDir = fileManager.homeDirectoryForCurrentUser
+    let originalFilePath = homeDir.appendingPathComponent("Library/Messages/chat.db").path
+    let copyFilePath = msgsBakDirPath + "chat_copy.db"
+
+    // Check if the copy file already exists and has the same modification date as the original file
+    if fileManager.fileExists(atPath: copyFilePath) {
+        do {
+            let originalAttributes = try fileManager.attributesOfItem(atPath: originalFilePath)
+            let copyAttributes = try fileManager.attributesOfItem(atPath: copyFilePath)
+            let originalModificationDate = originalAttributes[.modificationDate] as! Date
+            let copyModificationDate = copyAttributes[.modificationDate] as! Date
+            if originalModificationDate == copyModificationDate {
+                return copyFilePath
+            }
+        } catch {
+            fatalError("Failed to get file attributes: \(error)")
+        }
+    }
+
+    // remove any existing copy of the file
+    try? fileManager.removeItem(atPath: copyFilePath)
+
+    // attempt to copy the file to the Documents directory
+    do {
+        try fileManager.copyItem(atPath: originalFilePath, toPath: copyFilePath)
+
+        // set read permissions on the copied file
+        let fileAttributes = [FileAttributeKey.posixPermissions: NSNumber(value: 0o644)]
+        try fileManager.setAttributes(fileAttributes, ofItemAtPath: copyFilePath)
+
+        return copyFilePath
+    } catch {
+        fatalError("Failed to create chat database copy: \(error)")
+    }
+}
+
+class MessageSource {
+    private var msgsBakDirPath: String = ""
+    private var chatDBFilePath: String = ""
+    private var db: OpaquePointer?
+    
+    func open(msgsBakDirPath: String) {
+        self.msgsBakDirPath = msgsBakDirPath
+        self.chatDBFilePath = getChatDBCopyPath(toDir: msgsBakDirPath)
+        
+        guard sqlite3_open_v2(chatDBFilePath, &db, SQLITE_OPEN_READWRITE, nil) == SQLITE_OK else {
+            let errorMessage = String(cString: sqlite3_errmsg(db))
+            fatalError("Opening database \(chatDBFilePath): \(errorMessage)")
+        }
+        //defer { sqlite3_close(db) }
+    }
+    
+    func getMessagesFor(year: Int) -> [Message] {
+        var messages: [Message] = []
+        
+        let fileManager = FileManager.default
+        let handlesName = "chat_handles.json"
+        let handlesPath = msgsBakDirPath + handlesName
+        var idNamedHandles: [String:String] = [:]
+        if fileManager.fileExists(atPath: handlesPath) {
+            do {
+                let url = URL(fileURLWithPath: handlesPath)
+                let data = try Data(contentsOf: url)
+                idNamedHandles = try JSONSerialization.jsonObject(with: data, options: []) as! [String: String]
+            } catch {
+                fatalError("Reading database handles file \(handlesPath)")
+            }
+        }
+        
+        let chatDBFilePath = getChatDBCopyPath(toDir: msgsBakDirPath)
+        var db: OpaquePointer?
+        
+        guard sqlite3_open_v2(chatDBFilePath, &db, SQLITE_OPEN_READWRITE, nil) == SQLITE_OK else {
+            let errorMessage = String(cString: sqlite3_errmsg(db))
+            fatalError("Opening database \(chatDBFilePath): \(errorMessage)")
+        }
+        defer { sqlite3_close(db) }
+        
+        var statement: OpaquePointer?
+        let query = "SELECT rowid, id FROM handle;"
+        if sqlite3_prepare_v2(db, query, -1, &statement, nil) == SQLITE_OK {
+            defer { sqlite3_finalize(statement) }
+            
+            var handles = [Int: String]()
+            while sqlite3_step(statement) == SQLITE_ROW {
+                let rowid = Int(sqlite3_column_int(statement, 0))
+                let id = String(cString: sqlite3_column_text(statement, 1))
+                handles[rowid] = idNamedHandles[id, default: id]
+            }
+            
+            let messageQuery = """
+            SELECT rowid, datetime(substr(date, 1, 9) + 978307200, 'unixepoch',
+            'localtime') AS f_date, guid, is_from_me, cache_has_attachments,
+            handle_id, text, service
+            FROM message ORDER BY f_date;
+            """
+            var statement2: OpaquePointer?
+            if sqlite3_prepare_v2(db, messageQuery, -1, &statement2,
+                                  nil) == SQLITE_OK {
+                defer { sqlite3_finalize(statement2) }
+                
+                while sqlite3_step(statement2) == SQLITE_ROW {
+                    let rowid = Int(sqlite3_column_int(statement2, 0))
+                    let dateString = String(cString: sqlite3_column_text(statement2, 1))
+                    let guid = String(cString: sqlite3_column_text(statement2, 2))
+                    let isFromMe = sqlite3_column_int(statement2, 3) == 1
+                    let hasAttach = sqlite3_column_int(statement2, 4) == 1
+                    let handleId = Int(sqlite3_column_int(statement2, 5))
+                    var text: String? = nil
+                    if let t = sqlite3_column_text(statement2, 6) {
+                        text = String(cString: t)
+                    }
+                    let svc = String(cString: sqlite3_column_text(statement2, 7))
+                    
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+                    let date = dateFormatter.date(from: dateString)!
+                    let who = handles[handleId]
+
+                    let msg = Message(
+                        who: who,
+                        rowid: rowid,
+                        date: date,
+                        guid: guid,
+                        isFromMe: isFromMe,
+                        hasAttach: hasAttach,
+                        handleID: handleId,
+                        text: text,
+                        svc: svc
+                    )
+                    
+                    let calendar = Calendar.current
+                    if calendar.component(.year, from: date) != year {
+                        continue
+                    }
+                    
+                    if msg.handleID == 0 {
+                        msg.isFromMe = true
+                    }
+                    messages.append(msg)
+                }
+            }
+        }
+
+        return messages
+    }
+}
